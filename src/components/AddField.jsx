686a1712cd6212f7ef435a377be79bf27ddc7c2e
DVNst <dvn-studio@mail.ru>
2021-12-21 23:06:08 +0300

    todo-tutorial-1 complete

--- src/components/AddField.jsx
+++ src/components/AddField.jsx
@@ -1,18 +1,44 @@
-import { TextField, Button, Checkbox } from '@mui/material';
-import AddIcon from '@mui/icons-material/Add';
-import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
-import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import { useState } from "react";
+import { TextField, Button, Checkbox } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import RadioButtonUncheckedIcon from "@mui/icons-material/RadioButtonUnchecked";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
-export const AddField = () => {
+export const AddField = ({ addTask }) => {
+  const [task, setTask] = useState({text: '', copmlete: false});
+
+  const handlerTextField = (e) => {
+    setTask({...task, text: e.target.value});
+  }
+
+  const handlerCheckbox = (e) => {
+    setTask({...task, copmlete: e.target.checked});
+  }
+
+  const handlerClickAdd = () => {
+    if (task.text !== '') {
+      addTask(task);
+      setTask({text: '', copmlete: false});
+    }
+  }
+
   return (
     <div className="field">
       <Checkbox
+        onChange={handlerCheckbox}
         className="checkbox"
         icon={<RadioButtonUncheckedIcon />}
         checkedIcon={<CheckCircleIcon />}
+        checked={task.copmlete}
       />
-      <TextField placeholder="Введите текст задачи..." variant="standard" fullWidth />
-      <Button>
+      <TextField
+        onChange={handlerTextField}
+        placeholder="Введите текст задачи..."
+        variant="standard"
+        fullWidth
+        value={task.text}
+      />
+      <Button onClick={handlerClickAdd}>
         <AddIcon />
       </Button>
     </div>
