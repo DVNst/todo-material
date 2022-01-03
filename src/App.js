495f211b2e9ba6abf782058064488d9098430006
DVNst <dvn-studio@mail.ru>
2022-01-04 00:09:33 +0300

    add redux, redux-react

--- src/App.js
+++ src/App.js
@@ -1,85 +1,17 @@
-import { useEffect, useReducer } from 'react';
+// import { useEffect, useReducer } from 'react';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
 import { Paper, Divider, Button, List, Tabs, Tab } from '@mui/material';
+
 import { AddField } from './components/AddField';
 import { Item } from './components/Item';
-
-const reducer = (state, action) => {
-  if (action.type === 'ADD_TASK') {
-    const id = state.tasks.reduce((max, item) => item.id > max ? item.id : max, 0) + 1;
-    return {
-      ...state,
-      tasks: [...state.tasks, { ...action.payload, id: id }]
-    };
-  }
-
-  if (action.type === 'REMOVE_TASK') {
-    return {
-      ...state,
-      tasks: state.tasks.filter((task) => task.id !== action.payload)
-    };
-  }
-
-  if (action.type === 'TOOGLE_COMPLETED') {
-    return {
-      ...state,
-      tasks: state.tasks.map((task) => (
-        task.id === action.payload 
-          ? {
-              ...task,
-              complete: !task.complete
-            } 
-          : task
-      ))
-    };
-  }
-
-  if (action.type === 'TOOGLE_COMPLETED_ALL') {
-    return {
-      ...state,
-      tasks: state.tasks.map((task) => (
-        {
-          ...task,
-          complete: action.payload
-        }
-      ))
-    };
-  }
-
-  if (action.type === 'REMOVE_ALL') {
-    return {
-      ...state,
-      tasks: []
-    };
-  }
-
-  if (action.type === 'SET_FILTER') {
-    return {
-      ...state,
-      filterBy: action.payload
-    };
-  }
-
-  if (action.type === 'SET_COMPLETED_ALL') {
-    return {
-      ...state,
-      completedAll: action.payload
-    };
-  }
-
-  return state;
-}
 
 const FILTER_INDEX = ['all', 'active', 'completed'];
 
 function App() {
-  const [state, dispatch] = useReducer(
-    reducer,
-    {
-      filterBy: 'all',
-      completedAll: false,
-      tasks: [{ text: 'test1', id: 1, complete: true }, { text: 'test2', id: 2, complete: false }, { text: 'test3', id: 3, complete: false }],
-    }
-  );
+  const state = useSelector(state => state);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     //проверка completedAll, чтобы лишний раз не менять
@@ -88,7 +20,7 @@ function App() {
     } else if (state.tasks.some((task) => task.complete === false) && state.completedAll) {
       dispatch({ type: 'SET_COMPLETED_ALL', payload: false});
     }
-  }, [state]);
+  }, [state, dispatch]);
 
   const addTask = (newTask) => {
     dispatch({ type: 'ADD_TASK', payload: newTask });
